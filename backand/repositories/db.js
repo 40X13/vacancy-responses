7460d0aab676d036b685da13adf2296e344eb426
Pavel Arlou <arloupav@gmail.com>
2024-11-26 15:21:32 +0300

    add

--- backand/repositories/db.js
+++ backand/repositories/db.js
@@ -1,7 +1,6 @@
 import {MongoClient} from 'mongodb';
 
 const url = process.env.MONGO_URL || 'mongodb://localhost:27017';
-//// mongodb://mongo:GMPATTBwFjRXzZfYzaozZtYxJIRBqaNA@junction.proxy.rlwy.net:42771
 const client = new MongoClient(url);
 
 const dbName = 'vacancy-responses';
